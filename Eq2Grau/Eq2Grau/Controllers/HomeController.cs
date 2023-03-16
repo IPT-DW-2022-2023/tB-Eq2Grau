23f576acf8513749f69e74198d5e5cb37a8243de
jcnpereira <casimiro@ipt.pt>
2023-03-16 16:45:25 +0000

    início da formatação do projeto

--- Eq2Grau/Eq2Grau/Controllers/HomeController.cs
+++ Eq2Grau/Eq2Grau/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 
 namespace Eq2Grau.Controllers {
     public class HomeController : Controller {
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger) {
